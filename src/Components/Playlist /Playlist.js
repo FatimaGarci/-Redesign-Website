edfb1d86063b32f192bc63a88f10120696f6ac05
rodriguezr18 <rodriguez.rudy219@gmail.com>
2021-02-15 17:10:29 -0500

    First Commit -Rudy

---
+++ src/Components/Playlist /Playlist.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './Playlist.css'
+
+class Playlist extends Component {
+    render(){
+        return(
+            <div>
+            </div>
+        )
+    }
+}
+
+export default Playlist
